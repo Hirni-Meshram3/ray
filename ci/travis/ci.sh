59dcab9e9874a5c2ce584adfa5508e1c30dfc389
Hirni-Meshram2 <79629201+Hirni-Meshram2@users.noreply.github.com>
2021-04-29 11:00:31 +0530

    Update ci.sh

--- ci/travis/ci.sh
+++ ci/travis/ci.sh
@@ -285,6 +285,7 @@ _bazel_build_before_install() {
     target="//:ray_pkg"
   fi
   # NOTE: Do not add build flags here. Use .bazelrc and --config instead.
+  ./configure
   sudo bazel build "${target}"
 }
 
